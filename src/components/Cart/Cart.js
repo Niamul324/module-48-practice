c4494b01517c04d759d6c56e6795524fcddf6e08
Nahid <nhnahid.du@gmail.com>
2023-09-17 12:09:22 +0600

    3nd commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cart = ({cart}) => {
     return (
-        <div>
+        <div className="cart-summary-container">
               <h4>Cart summary</h4>
                 <p>Total Cart : {cart.length}</p>
         </div>
